1e7282ac4725bbe708f24fda3d58c71920b3e711
michiyamamoto8910 <michihiroyamamoto123@gmail.com>
2019-06-21 17:33:43 +0800

    6/21/19

--- js/myJScript.js
+++ js/myJScript.js
@@ -1,21 +1,3 @@
-$(document).ready(function(){
-  var storage=window.localStorage
-  $(`#mainform`).on(`submit`,function(e){
-    e.preventDefault()
-    var wherefrom=$(`#from`).val()
-    //storage.setItem('from', wherefrom)
-    var whereTo=$(`#to`).val()
-    var whendepart=$(`#depart`).val()
-    var whenreturn=$(`#return`).val()
-    $(`#displayfrom`).text(wherefrom)
-    $(`#displayto`).text(whereTo)
-    $(`#displaydepart`).text(whendepart)
-    $(`#displayreturn`).text(whenreturn)
-  })
-  //var storedFrom=storage.getItem('from')
-  //$(`#displayfrom`).text(storedFrom)
-  // storage.clear()
-})
 $(document).ready(function(e){
   var storage=window.localStorage
   $(`#loginform`).on(`submit`,function(e){
@@ -25,6 +7,50 @@ $(document).ready(function(e){
   })
   var greeting = 'Welcome!'
   var emptyusername='Guest'
+  let welcomemessage='Search flights below'
+  if(storage.getItem('username')){
+    welcomemessage="It's good to have you back"
+  }
   var storedUsername=storage.getItem('username')||emptyusername
   $(`#welcomeuser`).text(greeting+' '+storedUsername)
+  $(`#welcomemessage`).text(welcomemessage)
+  storage.clear()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // $(document).ready(function(){
+// //   var storage=window.localStorage
+// //   $(`#mainform`).on(`submit`,function(e){
+// //     e.preventDefault()
+// //     var wherefrom=$(`#from`).val()
+// //     //storage.setItem('from', wherefrom)
+// //     var whereTo=$(`#to`).val()
+// //     var whendepart=$(`#depart`).val()
+// //     var whenreturn=$(`#return`).val()
+// //     $(`#displayfrom`).text(wherefrom)
+// //     $(`#displayto`).text(whereTo)
+// //     $(`#displaydepart`).text(whendepart)
+// //     $(`#displayreturn`).text(whenreturn)
+// //   })
+//   //var storedFrom=storage.getItem('from')
+//   //$(`#displayfrom`).text(storedFrom)
+//   // storage.clear()
+// })
