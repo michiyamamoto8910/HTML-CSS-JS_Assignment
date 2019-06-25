03b9e9b214064395a2e3bd41545eb68def3217ad
michiyamamoto8910 <michihiroyamamoto123@gmail.com>
2019-06-25 17:40:13 +0800

    6/25/2019

--- js/myJScript.js
+++ js/myJScript.js
@@ -1,9 +1,9 @@
+//for getting,displaying,and checking the username
 $(document).ready(function(e){
   var storage=window.localStorage
   $(`#loginform`).on(`submit`,function(e){
     var username=$(`#username`).val()
     storage.setItem('username',username)
-    console.log(username);
   })
   var greeting = 'Welcome!'
   var emptyusername='Guest'
@@ -16,6 +16,48 @@ $(document).ready(function(e){
   $(`#welcomemessage`).text(welcomemessage)
 })
 
+//for getting and displaying number of travellers and cabin class
+$(document).ready(function(e){
+  var storage=window.localStorage
+  $(`#search-form`).on(`submit`,function(e){
+
+    var numberOfAdults = $(`#number-of-adults`).val()
+    var numberOfInfant = $(`#number-of-infants`).val()
+    var numberOfChild = $(`#number-of-child`).val()
+    var cabinClass = $(`#cabin`).val()
+    storage.setItem('cabin-class',cabinClass)
+    storage.setItem('number-of-adults',numberOfAdults)
+    storage.setItem('number-of-infants',numberOfInfant)
+    storage.setItem('number-of-child',numberOfChild)
+
+  })
+  var adultNumber = storage.getItem('number-of-adults')
+  var infantNumber = storage.getItem('number-of-infants')
+  var childNumber = storage.getItem('number-of-child')
+  var cabinClass = storage.getItem('cabin-class')
+  let adult=" Adult,"
+  let child= " Child,"
+  let infant=" Infant"
+
+  if(adultNumber < 1){
+    adult=""
+  }
+  if(infantNumber < 1){
+    infant=""
+    child=" Child"
+  }
+  if(childNumber < 1){
+    child=""
+    adult=" Adult"
+  }
+
+  if(childNumber < 1 && infantNumber >= 1){
+    adult = " Adult,"
+  }
+
+  $(`.show-homies`).text(adultNumber + adult + childNumber + child + infantNumber + infant )
+  $(`.show-cabin`).text(cabinClass + " Class")
+})
 
 
 
